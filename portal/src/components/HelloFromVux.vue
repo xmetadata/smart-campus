3d775161d295e40e0740f5b2d8de9e7166c2d4b7
duyajun <717094676@qq.com>
2018-02-25 12:17:39 +0800

    Initial Construct

---
+++ portal/src/components/HelloFromVux.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="vux-demo">
+      <img class="logo" src="../assets/vux_logo.png">
+      <h1> </h1>
+    </div>
+    <group title="cell demo">
+      <cell title="VUX" value="cool" is-link></cell>
+      <x-input title="title" v-model="xi"></x-input>
+      <x-number title="title" v-model="xn"></x-number>
+    </group>
+  </div>
+</template>
+
+<script>
+import { Group, Cell, XInput, XNumber } from 'vux'
+
+export default {
+  components: {
+    Group,
+    Cell,
+    XInput,
+    XNumber
+  },
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello World!',
+      xi: '',
+      xn: 0
+    }
+  }
+}
+</script>
+
+<style>
+.vux-demo {
+  text-align: center;
+}
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
