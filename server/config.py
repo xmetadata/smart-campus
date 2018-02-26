634a2686de72cea859161d309e056fdc43734e49
Du.Yajun <717094676@qq.com>
2018-02-26 11:39:58 +0800

    Update config.py

--- server/config.py
+++ server/config.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 # SERVER
 DEBUG = True
 PORT = 5000
-HOST = "0.0.0.0"
+HOST = "127.0.0.1"
 
 # JWT
 SECRET_KEY = "SOME SECRET"
@@ -11,9 +11,9 @@ JWT_EXPIRATION_DELTA = timedelta(seconds=3600)
 
 # MYSQL
 DB_USER = 'root'
-DB_PASS = 'Aa888888'
+DB_PASS = ''
 DB_NAME = 'smart-campus'
-DB_ADDR = '106.14.174.55'
+DB_ADDR = 'localhost'
 
 SQLALCHEMY_ECHO = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
