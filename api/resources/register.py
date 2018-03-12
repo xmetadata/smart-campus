a00b5a335e479a08ce3c50f35278991e6612d1f1
MaybeRusty <1421819341@qq.com>
2018-03-12 22:53:07 +0800

    微调

--- api/resources/register.py
+++ api/resources/register.py
@@ -7,11 +7,13 @@ import json
 class Register(Resource):
     def post(self):
         req_json = json.dumps(request.get_json())
+        print "@@@@@@@@@@@@@@@@@@@@"
+        print req_json
         try:
             load_data, errors = LoginSchema().loads(req_json)
             if errors:
                 return errors, 400
-            new_user = Admin(phone_number=load_data['phone_number'], password=load_data['password'], role=2)
+            new_user = Admin(phone_number=load_data['phone_number'], password=load_data['password'], role= 1 if load_data['user_type'] == 'wechat' else 2)
             new_user.add(new_user)
         except SQLAlchemyError as e:
             return e.message, 500
