390702fedbfa3f575cbc453519395434d56a247b
MaybeRusty <1421819341@qq.com>
2018-03-15 23:37:15 +0800

    删除未使用的接口

--- api/app.py
+++ api/app.py
@@ -4,8 +4,6 @@ from flask_restful import Api
 from common.database import db
 from common.schema import ma
 from common.security import authenticate, identity, payload_handle, auth_url_rule, auth_url_options
-from resources.item import ItemList, ItemDetail
-from resources.user import UserList, UserDetail
 from resources.register import Register
 from resources.admin import AdminData
 from resources.basicdata import BasicList, BasicEdit
@@ -23,10 +21,6 @@ jwt.jwt_payload_handler(payload_handle)
 app.add_url_rule(auth_url_rule, **auth_url_options)
 
 
-api.add_resource(ItemDetail, '/user/<int:user_id>/item/<int:id>')
-api.add_resource(ItemList, '/user/<int:user_id>/item')
-api.add_resource(UserList, '/user')
-api.add_resource(UserDetail, '/user/<int:id>')
 api.add_resource(AdminData, '/admin')
 api.add_resource(Register, '/register')
 api.add_resource(BasicList, '/BasicList/<string:node_uuid>')
